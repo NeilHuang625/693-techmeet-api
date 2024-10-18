b96528deac48b202961066b3166345a99569ca5a
neiltnag625 <neiltang618@gmail.com>
2024-10-18 15:24:46 +1300

    notification update every 12 hours

--- techmeet-api/BackgroundTasks/NotificationBackgroundService.cs
+++ techmeet-api/BackgroundTasks/NotificationBackgroundService.cs
@@ -36,7 +36,7 @@ namespace techmeet_api.BackgroundTasks
                     await GenerateNotificationsAsync(context);
                 }
 
-                await Task.Delay(TimeSpan.FromSeconds(30), stoppingToken);
+                await Task.Delay(TimeSpan.FromHours(12), stoppingToken);
             }
         }
 
